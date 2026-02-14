c19748ef5d83bdb202e9370d8713d779a637ea96
HeFidelis <heitorfidelis81@gmail.com>
2026-02-14 16:44:56 -0400

    Adicionando ação no component

--- src/app/calculadora/calculadora.ts
+++ src/app/calculadora/calculadora.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Calculadora {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    console.log("Chamando o método para calcular o resultado!");
+    console.log("Numero1: " + this.numero1);
+    console.log("Numero2: " + this.numero2);
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
